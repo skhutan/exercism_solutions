fd17511e2be08326de545a1ba238ca6299c41078
Sunil Khutan <sunil.khutan@bbc.co.uk>
2016-03-30 13:51:56 +0100

    finished all steps for a working run length encoding

--- run-length-encoding/rle.exs
+++ run-length-encoding/rle.exs
@@ -8,7 +8,23 @@ defmodule RunLengthEncoder do
   """
   @spec encode(String.t) :: String.t
   def encode(string) do
-
+    string
+    |> String.split("", trim: true)
+    |> Enum.reduce("", fn(letter, acc) ->
+      if String.last(acc) === letter do
+        
+        #get last number by regex
+        re = ~r/(\d+)[A-Z]$/
+        encode_number = Regex.run(re, acc, capture: :all_but_first) |> List.first
+        Regex.replace(re, acc, increment_encoding(encode_number) <> letter)
+      else
+        acc <> "1" <> letter
+      end
+      end)
+  end
+  
+  defp increment_encoding(encode_number) do
+    Integer.to_string(String.to_integer(encode_number) + 1)
   end
 
   @spec decode(String.t) :: String.t
