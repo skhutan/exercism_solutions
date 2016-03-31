9b7d0c40ab32c9da90ac6f6afd0923229ac4959c
Sunil Khutan <sunil.khutan@bbc.co.uk>
2016-03-31 11:29:51 +0100

    cleaned up function declarations

--- list-ops/list_ops.exs
+++ list-ops/list_ops.exs
@@ -7,50 +7,28 @@ defmodule ListOps do
   # automatically imported) and so shouldn't be used either.
 
   @spec count(list) :: non_neg_integer
-  def count(l) do
-    _count(l, 0)
-  end
+  def count(l), do: _count(l, 0)
   
-  defp _count([], acc) do
-    acc
-  end
-  defp _count([h|t], acc) do
-    _count(t, acc+1)
-  end
+  defp _count([], acc), do: acc
+  defp _count([h|t], acc), do: _count(t, acc+1)
 
   @spec reverse(list) :: list
-  def reverse(l) do
-    _reverse(l, [])
-  end
+  def reverse(l), do: _reverse(l, [])
   
-  defp _reverse([], acc) do
-    acc
-  end
-  defp _reverse([h|t], acc) do
-    _reverse(t, [h|acc])
-  end
+  defp _reverse([], acc), do: acc
+  defp _reverse([h|t], acc), do: _reverse(t, [h|acc])
 
   @spec map(list, (any -> any)) :: list
-  def map(l, f) do
-    _map(l, f, [])
-  end
-  
-  defp _map([], f, acc) do
-    reverse(acc)
-  end
-  defp _map([h|t], f, acc) do
-    _map(t, f, [f.(h) | acc])
-  end
+  def map(l, f), do: _map(l, f, [])
+
+  defp _map([], f, acc), do: reverse(acc)
+  defp _map([h|t], f, acc), do: _map(t, f, [f.(h) | acc])
+
 
   @spec filter(list, (any -> as_boolean(term))) :: list
-  def filter(l, f) do
-    _filter(l, f, [])
-  end
+  def filter(l, f), do: _filter(l, f, [])
   
-  defp _filter([], f, acc) do
-    reverse(acc)
-  end
-  
+  defp _filter([], f, acc), do: reverse(acc)
   defp _filter([h|t], f, acc) do
     if f.(h) do
       _filter(t, f, [h | acc])
@@ -64,6 +42,8 @@ defmodule ListOps do
   def reduce(l, acc, f) do
 
   end
+  
+  
 
   @spec append(list, list) :: list
   def append(a, b) do
