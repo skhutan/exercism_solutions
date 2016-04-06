9fd4876969818869c283e1151b0a71a166edb412
Sunil Khutan <sunil.khutan@bbc.co.uk>
2016-04-06 09:27:04 +0100

    updated rna transcription to use map

--- rna-transcription/dna.exs
+++ rna-transcription/dna.exs
@@ -1,6 +1,4 @@
 defmodule DNA do
-  @transcriptor %{G: "C", C: "G", T: "A", A: "U"}
-  
   @doc """
   Transcribes a character list representing DNA nucleotides to RNA
 
@@ -11,12 +9,7 @@ defmodule DNA do
   """
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
-    dna
-    |> List.to_string
-    |> String.graphemes
-    |> Enum.reduce("", &build_rna/2)
-    |> String.to_char_list
+    transcriptor = %{?G => ?C, ?C => ?G, ?T => ?A, ?A => ?U}
+    Enum.map(dna, &(transcriptor[&1]))
   end
-  
-  defp build_rna(nuc, acc), do: acc <> @transcriptor[String.to_atom(nuc)]
 end
