610afd3ad00818482d6591ec7a1acc284e0a467a
Sunil Khutan <sunil.khutan@bbc.co.uk>
2017-05-09 15:46:41 +0100

    implemented simple linked list

---
+++ elixir/simple-linked-list/linked_list.exs
@@ -0,0 +1,69 @@
+defmodule LinkedList do
+  @opaque t :: tuple()
+
+  @doc """
+  Construct a new LinkedList
+  """
+  @spec new() :: t
+  def new(), do: {}
+
+  @doc """
+  Push an item onto a LinkedList
+  """
+  @spec push(t, any()) :: t
+  def push(list, elem), do: {elem, list}
+
+  @doc """
+  Calculate the length of a LinkedList
+  """
+  @spec length(t) :: non_neg_integer()
+  def length({}), do: 0
+  def length({_, t}), do: 1 + LinkedList.length(t)
+
+  @doc """
+  Determine if a LinkedList is empty
+  """
+  @spec empty?(t) :: boolean()
+  def empty?({}), do: true
+  def empty?(_), do: false
+
+  @doc """
+  Get the value of a head of the LinkedList
+  """
+  @spec peek(t) :: {:ok, any()} | {:error, :empty_list}
+  def peek({}), do: {:error, :empty_list}
+  def peek({h, _}), do: {:ok, h}
+
+  @doc """
+  Get tail of a LinkedList
+  """
+  @spec tail(t) :: {:ok, t} | {:error, :empty_list}
+  def tail({}), do: {:error, :empty_list}
+  def tail({_, t}), do: {:ok, t}
+
+  @doc """
+  Remove the head from a LinkedList
+  """
+  @spec pop(t) :: {:ok, any(), t} | {:error, :empty_list}
+  def pop({}), do: {:error, :empty_list}
+  def pop({h, t}), do: {:ok, h, t}
+
+  @doc """
+  Construct a LinkedList from a stdlib List
+  """
+  @spec from_list(list()) :: t
+  def from_list(list), do: List.foldr(list, LinkedList.new(), &(LinkedList.push(&2, &1)))
+
+  @doc """
+  Construct a stdlib List LinkedList from a LinkedList
+  """
+  @spec to_list(t) :: list()
+  def to_list({}), do: []
+  def to_list({h, t}), do: [h|to_list(t)]
+
+  @doc """
+  Reverse a LinkedList
+  """
+  @spec reverse(t) :: t
+  def reverse(list), do: list |> LinkedList.to_list |> Enum.reverse |> LinkedList.from_list
+end
