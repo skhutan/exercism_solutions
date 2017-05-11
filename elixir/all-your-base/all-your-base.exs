7d1a2d8bab21e1309cb44129128145273fcc44f3
Sunil Khutan <sunil.khutan@bbc.co.uk>
2017-05-11 11:41:13 +0100

    implement all your base

---
+++ elixir/all-your-base/all-your-base.exs
@@ -0,0 +1,27 @@
+defmodule AllYourBase do
+  @doc """
+  Given a number in base a, represented as a sequence of digits, converts it to base b,
+  or returns nil if either of the bases are less than 2
+  """
+
+  @spec convert(list, integer, integer) :: list
+  def convert(digits, base_a, base_b)
+  when length(digits) == 0 or base_a < 2 or base_b < 2, do: nil
+
+  def convert(digits, base_a, base_b) do
+    case from_digits(digits, base_a, 0) do
+      {:ok, number} -> to_digits(number, base_b, [])
+      {:error, error} -> error
+    end
+  end
+
+  defp from_digits([], _, acc), do: {:ok, acc}
+  defp from_digits([h|_], b, acc) when h < 0 or h == b, do: {:error, nil}
+  defp from_digits([h|t], base, acc), do: from_digits(t, base, base * acc + h)
+
+  defp to_digits(n, base, acc) when n > 0 do
+    to_digits(Integer.floor_div(n, base), base, [rem(n, base)] ++ acc)
+  end
+  defp to_digits(0, _, acc) when length(acc) == 0, do: [0]
+  defp to_digits(0, _, acc), do: acc
+end
